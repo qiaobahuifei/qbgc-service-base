cf69f6d5a7da8a163399404617fcff4cc55cb4bf
gaochao <gaochao5kuba@163,com>
2019-01-31 17:34:05 +0800

    first commit

---
+++ src/main/java/com/qbgc/service/base/QbgcServiceBaseApplication.java
@@ -0,0 +1,14 @@
+package com.qbgc.service.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbgcServiceBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbgcServiceBaseApplication.class, args);
+	}
+
+}
+
